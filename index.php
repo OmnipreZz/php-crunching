b786d4f45b1cc3980b021961815c478d3dc0512e
Omniprezz <geoffrey.frioli@gmail.com>
2018-08-02 21:58:56 +0200

    add exception

--- index.php
+++ index.php
@@ -5,72 +5,95 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>php-crunching</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
     <script src="main.js"></script>
 </head>
 <body>
     
 
-    <h1 style="text-align:center">Php-crunching</h1>
+    <h1>Php-crunching</h1>
     <br><br>
+
+
+                    <!-- ////////////////EXERCICES DICTIONNAIRE//////////////// -->
+
     <h2>Exercices Dictionnaire</h2>
     <br>
     <?php
         $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
         $dico = explode("\n", $string);
         // var_dump($dico);
+
+
+        // exception qui test que le dico est bien chargé!!
+        function dicoLoad($string) {
+            if (!$string) {
+                throw new Exception('Dictionnaire Manquant!');
+            } else {
+                echo '<p style="color: red;">Dictionnaire bien chargé!</p><br>';
+            }
+            
+        }
+        try {
+            dicoLoad($string);
+        }catch (Exception $e) {
+            echo '<p style="color: red;">Message d\'erreur : ', $e->getMessage().'</p><br>';
+        }
     ?>
 
 
-    <p>Combien de mots contient ce dictionnaire ?</p>
+
+    <p class='line'>Combien de mots contient ce dictionnaire ?</p>
     <?php
-        echo 'il contient '.count($dico).' '.'mots.';
+        echo '<p>il contient '.count($dico).' '.'mots.</p>';
     ?>
     <br><br><br>
 
 
-    <p>Combien de mots font exactement 15 caractères ?</p>
+
+    <p class='line'>Combien de mots font exactement 15 caractères ?</p>
     <?php
-        $nbr = 0;
+        $count = 0;
         foreach ($dico as $value) {
             if (strlen($value) == 15) {
-               $nbr++; 
+               $count++; 
             }
         }
-        echo $nbr.' '.'mots font exactement 15 caractères.';
+        echo '<p>'.$count.' '.'mots font exactement 15 caractères.</p>';
     ?>
     <br><br><br>
 
 
 
-    <p>Combien de mots contiennent la lettre « w » ?</p>
+    <p class='line'>Combien de mots contiennent la lettre « w » ?</p>
     <?php
-        $nb = 0;
+        $count = 0;
         foreach ($dico as $value) {
-            if (strpos($value,  "w")) {
-                $nb += 1;
+            if (stristr($value,  "w")) {
+                $count++;
             }
         }
-        echo $nb.' '.'mots contiennent la lettre « w ».';
+        echo '<p>'.$count.' '.'mots contiennent la lettre « w ».</p>';
     ?>
     <br><br><br>
 
 
 
-    <p>Combien de mots finissent par la lettre « q » ?</p>
+    <p class='line'>Combien de mots finissent par la lettre « q » ?</p>
     <?php
-        $n = 0;
+        $count = 0;
         foreach ($dico as $value) {
-
             if ($value[strlen($value)-1] == "q") {
-                $n++;
+                $count++;
             }
         }
-        echo $n.' '.'mots finissent par la lettre « q ».';
+        echo '<p>'.$count.' '.'mots finissent par la lettre « q ».</p>';
     ?>
     <br><br><br>
 
 
+
+                        <!-- ////////////////EXERCICES LISTE DE FILMS//////////////// -->
 
     <h2>Exercices liste de films</h2>
     <br>
@@ -79,21 +102,30 @@
         $brut = json_decode($string, true);
         $top = $brut["feed"]["entry"]; # liste de films
         // var_dump($top);
+
+        //exception qui test que la liste de films est bien chargé!!
+        function filmLoad($string) {
+            if (!$string) {
+                throw new Exception('Liste de films manquante!');
+            } else {
+                echo '<p style="color: red;">Liste de films ok!</p><br>';
+            }
+            
+        }
+        try {
+            filmLoad($string);
+        }catch (Exception $e) {
+            echo '<p style="color: red;">Message d\'erreur : ', $e->getMessage().'</p><br>';
+        }
     ?>
 
 
 
-    <p>Afficher le top10 des films?</p>
+    <p class='line'>Afficher le top10 des films?</p>
     <?php
         foreach ($top as $index => $array) {
             if ($index < 9) {
-                foreach ($array as $key => $value) {
-                    if ($key == "title") {
-                        foreach ($value as $val) {
-                            echo $val.'<br>';
-                        }
-                    }
-                }
+                echo '<p>'.$array['title']['label'].'</p>'; 
             }
         }
     ?>
@@ -101,17 +133,11 @@
 
 
 
-    <p>Quel est le classement du film « Gravity » ?<p>
+    <p class='line'>Quel est le classement du film « Gravity » ?<p>
     <?php
-        foreach ($top as $index => $array) {
-            foreach ($array as $key => $value) {
-                if ($key == "title") {
-                    foreach ($value as $val) {
-                        if (substr($val, 1, 6) == "ravity") {
-                            echo 'le classement de Gravity est '.$index.'.';
-                        }
-                    }
-                }
+        foreach ($top as $key => $array) {
+            if($array['im:name']['label'] == "Gravity") {
+                echo '<p>le classement de Gravity est '.$key.'eme.</p>';
             }
         }
     ?>
@@ -119,7 +145,7 @@
 
 
 
-    <p>Combien de films sont sortis avant 2000 ?<p>
+    <p class='line'>Combien de films sont sortis avant 2000 ?<p>
     <?php
         $count = 0;
         foreach ($top as $key => $value) {
@@ -127,26 +153,24 @@
                 $count ++;
             }
         }
-        echo $count." "."films sont sortis avant 2000.";
+        echo '<p>'.$count." "."films sont sortis avant 2000.</p>";
     ?>
     <br><br><br>
 
 
 
-    <p>Quel est le film le plus récent ? Le plus vieux ?<p>
+    <p class='line'>Quel est le film le plus récent ? Le plus vieux ?<p>
     <?php
         $arr = [];
         foreach ($top as $key => $value) {
             $arr[$value["im:name"]["label"]] = substr($value["im:releaseDate"]["label"], 0, 10);
         }
-        $desc = max($arr);
-        $asc = min($arr);
         foreach ($arr as $key => $value) {
-            if ($value == $desc) {
-                echo 'le film le plus récent est : '.' '.$key.'<br>';
+            if ($value == max($arr)) {
+                echo '<p>le film le plus récent est : '.' '.$key.'</p>';
             }
-            if ($value == $asc) {
-                echo 'le film le plus vieux est : '.' '.$key;
+            if ($value == min($arr)) {
+                echo '<p>le film le plus vieux est : '.' '.$key.'</p>';
             }
         }
     ?>
@@ -154,17 +178,16 @@
 
 
 
-    <p>Quelle est la catégorie de films la plus représentée ?<p>
+    <p class='line'>Quelle est la catégorie de films la plus représentée ?<p>
     <?php
         $array = [];
         foreach ($top as $key => $value) {
             array_push($array, $value["category"]["attributes"]["label"]);
         }
         $arrMax = array_count_values($array);
-        $max = max($arrMax);
         foreach ($arrMax as $key => $value) {
-            if ($value == $max) {
-                echo 'la catégorie de films la plus représentée est '.$key.'.'.'<br>';
+            if ($value == max($arrMax)) {
+                echo '<p>la catégorie de films la plus représentée est '.$key.'.'.'</p><br>';
             }
         }
     ?>
@@ -172,17 +195,16 @@
 
 
 
-    <p>Quel est le réalisateur le plus présent dans le top100 ?<p>
+    <p class='line'>Quel est le réalisateur le plus présent dans le top100 ?<p>
     <?php
-        $array = [];
+        $arr = [];
         foreach ($top as $key => $value) {
-            array_push($array, $value["im:artist"]["label"]);
+            array_push($arr, $value["im:artist"]["label"]);
         }
-        $arrMax = array_count_values($array);
-        $max = max($arrMax);
+        $arrMax = array_count_values($arr);
         foreach ($arrMax as $key => $value) {
-            if ($value == $max) {
-                echo 'le réalisateur le plus présent dans le top100 est '.$key.'.'.'<br>';
+            if ($value == max($arrMax)) {
+                echo '<p>le réalisateur le plus présent dans le top 100 est '.$key.'.'.'</p><br>';
             }
         }
     ?>
@@ -190,21 +212,30 @@
 
 
 
-    <p>Combien cela coûterait-il d'acheter le top10 sur iTunes ? de le louer ?<p>
+    <p class='line'>Combien cela coûterait-il d'acheter le top10 sur iTunes ? de le louer ?<p>
     <?php
-        $ar = [];
+        $arr = [];
         foreach ($top as $key => $value) {
             if ($key < 10) {
-               array_push($ar, substr($value["im:price"]["label"], 1, 5)); 
+               array_push($arr, substr($value["im:price"]["label"], 1, 5)); 
             }
         }
-        echo "acheter le top10 coûterait ".array_sum($ar)."$.";
+        echo "<p>acheter le top10 coûterait ".array_sum($arr)."$.</p>";
+    ?>
+    <?php
+        $arr = [];
+        foreach ($top as $key => $value) {
+            if ($key < 10) {
+               array_push($arr, substr($value["im:rentalPrice"]["label"], 1, 5)); 
+            }
+        }
+        echo "<p>louer le top10 coûterait ".array_sum($arr)."$.</p>";
     ?>
     <br><br><br>
 
 
 
-    <p>Quel est le mois ayant vu le plus de sorties au cinéma ?<p>
+    <p class='line'>Quel est le mois ayant vu le plus de sorties au cinéma ?<p>
     <?php
         $mois = [];
         $cal = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
@@ -212,10 +243,9 @@
             array_push($mois, substr($value["im:releaseDate"]["label"], 5, 2));
         }
         $dateVal = array_count_values($mois);
-        $moisMax = max($dateVal);
         foreach ($dateVal as $key => $value) {
-            if ($value == $moisMax) {
-                echo "le mois ayant vu le plus de sorties au cinéma est le mois de ".$cal[(substr($key, 1, 1))-1]." "."avec ".$value." sorties.<br>";
+            if ($value == max($dateVal)) {
+                echo "<p>le mois ayant vu le plus de sorties au cinéma est le mois de ".$cal[(substr($key, 1, 1))-1]." "."avec ".$value." sorties.</p>";
             }
         } 
     ?>
@@ -223,24 +253,24 @@
 
 
 
-    <p>Quels sont les 10 meilleurs films à voir en ayant un budget limité ?<p>
+    <p class='line'>Quels sont les 10 meilleurs films à voir en ayant un budget limité ?<p>
     <?php
         $prix = [];
+        $stopCount = 0;
         foreach ($top as $key => $value) {
             $prix[$value["im:name"]["label"]] = substr($value["im:price"]["label"], 1, 5);
         }
-        $minPrix = min($prix);;
+        asort($prix);
         foreach ($prix as $key => $value) {
-            if ($value == $minPrix) {
-                echo $key."<br>";
-            }
-        }
-        foreach ($top as $key => $value) {
-            if ($key < 6) {
-                echo $value["im:name"]["label"]."<br>";
+            echo '<p>'.$key.'</p>';
+            $stopCount++;
+            if ($stopCount == 10) {
+            break;
             }
         }
     ?>
+
+
 
 </body>
 </html>
