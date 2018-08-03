7f68f6a014e0a243cbc404cd9114f907c6420437
Omniprezz <geoffrey.frioli@gmail.com>
2018-08-03 14:26:56 +0200

    modif test

--- tests/test.php
+++ tests/test.php
@@ -3,16 +3,12 @@
 require "./index.php";
 
 
-
-
 class codeTest extends \PHPUnit\Framework\TestCase {
     
     function testTopTen() {
-        $result = topTen($GLOBALS['top']);
-        $this->assertCount(10, $result);
+        $this->assertCount(10, topTen());
     }
 }
-
 
 
 ?>
